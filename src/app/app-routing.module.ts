53d2bc6ab2dc544274d44711d5038355f01e8303
Paul Chesa <paulchesa1@gmail.com>
2020-10-15 08:55:09 +0300

    started design, navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component: HomeComponent}, 
+  {path:"", redirectTo: "/home" , pathMatch: "full"},
+  {path:"**", component: ErrorPageComponent},
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbCollapseModule   
   ],
   providers: [],
   bootstrap: [AppComponent]
