bbc9716b2b95d4b70067c5628b952029117ad872
Paul Chesa <paulchesa1@gmail.com>
2020-11-04 09:35:54 +0300

    Created orders model

---
+++ src/app/libs/models/orders-model.spec .ts
@@ -0,0 +1,7 @@
+import {OrdersModel } from './orders-model';
+
+describe('OrdersModel', () => {
+  it('should create an instance', () => {
+    expect(new OrdersModel()).toBeTruthy();
+  });
+});
---
+++ src/app/libs/models/orders-model.ts
@@ -0,0 +1,8 @@
+export class OrdersModel {
+    orderNo: string;
+    products: [];
+    email: string;
+    phoneNumber: number;
+    firstName: string;
+    lastName: string;
+}
