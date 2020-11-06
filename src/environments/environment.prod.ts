2693aa1f346fafce3c71570e1be679e3f9b49011
Paul Chesa <paulchesa1@gmail.com>
2020-11-06 19:26:28 +0300

    change environement config for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyB9kqZiok0XWdJsMt7oZfEohTRyleN09vI",
+    authDomain: "sports-store-c1ca4.firebaseapp.com",
+    databaseURL: "https://sports-store-c1ca4.firebaseio.com",
+    projectId: "sports-store-c1ca4",
+    storageBucket: "sports-store-c1ca4.appspot.com",
+    messagingSenderId: "336982438490",
+    appId: "1:336982438490:web:9c386d2d65a0b80afa9e63",
+    measurementId: "G-FS0F4WWZYX"
+  }
 };
