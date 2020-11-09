1aa094c88aa8f39ece3d0de87ee60106e4cdd503
Paul Chesa <paulchesa1@gmail.com>
2020-11-09 11:28:31 +0300

    update package json

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
     apiKey: process.env.API_KEY,
     authDomain:  process.env.AUTH_DOMAIN,
     databaseURL:  process.env.DATABASE_URL,
-    projectId: "sports-store-c1ca4",
+    projectId:  "sports-store-c1ca4",
     storageBucket:  process.env.STORAGE_BUCKET,
     messagingSenderId:  process.env.MESSAGING_SENDER_ID,
     appId:  process.env.API_ID,
