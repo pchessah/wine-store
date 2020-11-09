723da25c1780a608e86ad25890a90c31f2be2a8f
Paul Chesa <paulchesa1@gmail.com>
2020-11-09 10:58:26 +0300

    added dist folder

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,13 @@
-
 export const environment = {
-   production: true,
-   firebaseConfig :{
+  production: true,
+  firebaseConfig :{
     apiKey: process.env.API_KEY,
     authDomain:  process.env.AUTH_DOMAIN,
     databaseURL:  process.env.DATABASE_URL,
-    projectId:  "sports-store-c1ca4",
+    projectId: "sports-store-c1ca4",
     storageBucket:  process.env.STORAGE_BUCKET,
     messagingSenderId:  process.env.MESSAGING_SENDER_ID,
     appId:  process.env.API_ID,
     measurementId:  process.env.MEASUREMENT_ID
-}
-}
+  }
+};
