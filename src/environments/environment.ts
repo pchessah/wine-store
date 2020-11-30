1005affff8140a078d7623f6185c81e316d3fdbe
Paul Chesa <paulchesa1@gmail.com>
2020-11-30 12:18:56 +0300

    Admin posting storage bucket error fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
     authDomain:  process.env.AUTH_DOMAIN,
     databaseURL:  process.env.DATABASE_URL,
     projectId:  "sports-store-c1ca4",
-    storageBucket:  process.env.STORAGE_BUCKET,
+    storageBucket:  "sports-store-c1ca4.appspot.com",
     messagingSenderId:  process.env.MESSAGING_SENDER_ID,
     appId:  process.env.API_ID,
     measurementId:  process.env.MEASUREMENT_ID
