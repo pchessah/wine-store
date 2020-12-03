b889c40d572089e11923082ac426759961c69483
Paul Chesa <paulchesa1@gmail.com>
2020-12-03 21:52:57 +0300

    prettier formarter

--- src/app/libs/services/products.service.ts
+++ src/app/libs/services/products.service.ts
@@ -1,93 +1,101 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { Injectable, OnInit } from '@angular/core';
-import { BehaviorSubject, Subject, throwError } from 'rxjs';
-import { catchError } from "rxjs/operators";
-import { AngularFirestore } from '@angular/fire/firestore';
-import { ProductsModel } from "../models/products-model";
-import { AngularFireStorage } from '@angular/fire/storage';
-import { ToastrService } from 'ngx-toastr';
+import { HttpErrorResponse } from '@angular/common/http'
+import { Injectable, OnInit } from '@angular/core'
+import { BehaviorSubject, Subject, throwError } from 'rxjs'
+import { catchError } from 'rxjs/operators'
+import { AngularFirestore } from '@angular/fire/firestore'
+import { ProductsModel } from '../models/products-model'
+import { AngularFireStorage } from '@angular/fire/storage'
+import { ToastrService } from 'ngx-toastr'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService implements OnInit {
-
   singleProductFromQuery: any
-  products: ProductsModel[];
-  singleProduct: ProductsModel = undefined;
+  products: ProductsModel[]
+  singleProduct: ProductsModel = undefined
   cart: ProductsModel[] = []
-  private singleProductSource = new BehaviorSubject<ProductsModel>(null);
-  cartSource = new BehaviorSubject<ProductsModel[]>([]);
-  currentCart = this.cartSource.asObservable();
-  currentSingleProduct = this.singleProductSource.asObservable();
+  private singleProductSource = new BehaviorSubject<ProductsModel>(null)
+  cartSource = new BehaviorSubject<ProductsModel[]>([])
+  currentCart = this.cartSource.asObservable()
+  currentSingleProduct = this.singleProductSource.asObservable()
 
-  constructor(private firestore: AngularFirestore,
-              private fireStorage: AngularFireStorage,
-              private toastr: ToastrService) { }
+  constructor(
+    private firestore: AngularFirestore,
+    private fireStorage: AngularFireStorage,
+    private toastr: ToastrService,
+  ) {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
-
-  
   //ADD ITEM TO CART
   addToCart(id) {
     this.getSingleProduct(id).subscribe((singleProduct: ProductsModel) => {
       if (this.singleProduct !== null || this.singleProduct !== undefined) {
-        this.singleProduct = singleProduct;
-        this.updateSingleProduct(this.singleProduct);
-        this.toastr.success(`${this.singleProduct.productName} added to cart`);
-        this.cart = [...this.cart, this.singleProduct];
-        this.updateCart(this.cart);
+        this.singleProduct = singleProduct
+        this.updateSingleProduct(this.singleProduct)
+        this.toastr.success(`${this.singleProduct.productName} added to cart`)
+        this.cart = [...this.cart, this.singleProduct]
+        this.updateCart(this.cart)
       }
     })
   }
 
-
   //CLEAR CART
   clearCart() {
-    this.cart = [];
-    this.updateCart(this.cart);
-    this.toastr.error("Cart has been cleared")
+    this.cart = []
+    this.updateCart(this.cart)
+    this.toastr.error('Cart has been cleared')
   }
 
   //CLEAR CART AFTER SUBMIT
-  submitCart(){
-    this.cart = [];
+  submitCart() {
+    this.cart = []
     this.updateCart(this.cart)
   }
 
-
   //ADD ITEM BASED ON PRODUCT ID QUERY
   async addOneItemToCart(id: string) {
-    const query = await this.firestore.firestore.collection("products").where("productId", "==", id).get();
+    const query = await this.firestore.firestore
+      .collection('products')
+      .where('productId', '==', id)
+      .get()
     if (!query.empty) {
-      const snapshot = query.docs[0];
-      const data = snapshot.data();
-      this.singleProductFromQuery = data;
-      this.toastr.success(`One more ${this.singleProductFromQuery.productName} added to cart.`)
-      this.cart = [...this.cart, this.singleProductFromQuery];
+      const snapshot = query.docs[0]
+      const data = snapshot.data()
+      this.singleProductFromQuery = data
+      this.toastr.success(
+        `One more ${this.singleProductFromQuery.productName} added to cart.`,
+      )
+      this.cart = [...this.cart, this.singleProductFromQuery]
       this.updateCart(this.cart)
     } else {
-      console.error("No item added");
+      console.error('No item added')
     }
   }
 
   //REMOVE ITEM BASED ON PRODUCT ID QUERY
   async removeOneItemFromCart(id: string) {
-    const query = await this.firestore.firestore.collection("products").where("productId", "==", id).get();
+    const query = await this.firestore.firestore
+      .collection('products')
+      .where('productId', '==', id)
+      .get()
     if (!query.empty) {
-      const snapshot = query.docs[0];
-      const data = snapshot.data();
-      this.singleProductFromQuery = data;
-      this.toastr.warning(`One ${this.singleProductFromQuery.productName} removed from cart.`);
-      let index = this.cart.findIndex(item => item.productId == this.singleProductFromQuery.productId);
-      this.cart.splice(index,1);     
+      const snapshot = query.docs[0]
+      const data = snapshot.data()
+      this.singleProductFromQuery = data
+      this.toastr.warning(
+        `One ${this.singleProductFromQuery.productName} removed from cart.`,
+      )
+      let index = this.cart.findIndex(
+        (item) => item.productId == this.singleProductFromQuery.productId,
+      )
+      this.cart.splice(index, 1)
       this.updateCart(this.cart)
     } else {
-      console.error("No item removed");
+      console.error('No item removed')
     }
   }
-
 
   //UPDATE CART AS OBSERVABLE
   updateCart(cart: ProductsModel[]) {
@@ -99,10 +107,9 @@ export class ProductsService implements OnInit {
     this.singleProductSource.next(singleProduct)
   }
 
-
   //CHECK ERRORS WITH HTTP CALLS
   handleError(err: HttpErrorResponse) {
-    let errorMessage = "";
+    let errorMessage = ''
     //check if error is on the client side
     if (err.error instanceof ErrorEvent) {
       errorMessage = `An error occured: ${err.error.message}`
@@ -110,36 +117,36 @@ export class ProductsService implements OnInit {
       //else error is server side
       errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`
     }
-    console.error(errorMessage);
-    return throwError(errorMessage);
+    console.error(errorMessage)
+    return throwError(errorMessage)
   }
-
 
   //GET ALL PRODUCTS
   getAllProducts() {
-    return this.firestore.collection("products").snapshotChanges().pipe(
-      catchError(this.handleError)
-    );
+    return this.firestore
+      .collection('products')
+      .snapshotChanges()
+      .pipe(catchError(this.handleError))
   }
-
 
   //GET SINGLE ITEM VIA ID
   getSingleProduct(id: string) {
-    return this.firestore.collection("products").doc(id).valueChanges().pipe(
-      catchError(this.handleError)
-    );
+    return this.firestore
+      .collection('products')
+      .doc(id)
+      .valueChanges()
+      .pipe(catchError(this.handleError))
   }
 
   //ADD NEW PRODUCT TO FIREBASE
   addNewProduct(product: ProductsModel) {
-    return this.firestore.collection("products").add(product)
+    return this.firestore.collection('products').add(product)
   }
-
 
   //UPLOAD FUNCTION
   uploadSingleFile(event, n) {
-    const file = event.target.files[0];
-    const task = this.fireStorage.upload(`productImages/${n}`, file);
+    const file = event.target.files[0]
+    const task = this.fireStorage.upload(`productImages/${n}`, file)
     return task.snapshotChanges()
   }
 }
